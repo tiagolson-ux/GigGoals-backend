f3da3d684c0f2370e77554364048ae5a7d7314ab
Tia Noel <tiagolson@gmail.com>
2026-02-13 13:26:58 -0500

    added imports to controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,7 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
