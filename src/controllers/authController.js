50ed36129792438b792f3ffed41007361dc29ee2
Tia Noel <tiagolson@gmail.com>
2026-02-13 16:13:14 -0500

    debug

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,25 +1,16 @@
-import User from "../models/User.js";
-import bcrypt from "bcryptjs";
-
-// ===============================
-// Software T: Register New User
-// ===============================
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // Check if user already exists
     const userExists = await User.findOne({ email });
 
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create user
     const user = await User.create({
       name,
       email,
@@ -27,36 +18,6 @@ export const registerUser = async (req, res) => {
     });
 
     res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email
-    });
-
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// ===============================
-// Software T: Login User
-// ===============================
-export const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    res.json({
       _id: user._id,
       name: user.name,
       email: user.email
