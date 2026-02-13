dd483f5b1d056bdd6b54c7ca912e3925f730594c
Tia Noel <tiagolson@gmail.com>
2026-02-12 21:48:35 -0500

    defined port to server js

--- src/server.js
+++ src/server.js
@@ -2,16 +2,17 @@ import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
-// Software T: Load environment variables first
+//  T: Load environment variables first
 dotenv.config();
 
-// Software T: Connect to Mongo before starting server
+//  T: Connect to Mongo before starting server
 connectDB();
 
-// Software T: Define port
+// T: Define port
 const PORT = process.env.PORT || 5000;
 
-// Software T: Start Express server
+// T: Start Express server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
