3ff4f9edfa610bf70c4b5255047ad3dad4e983ab
Tia Noel <tiagolson@gmail.com>
2026-02-13 16:26:41 -0500

    added import

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,10 +3,7 @@ import { registerUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
-// Software T: register route
 router.post("/register", registerUser);
-
-// Software T: login route
 router.post("/login", loginUser);
 
 export default router;
