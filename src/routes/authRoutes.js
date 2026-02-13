19f2a06596544f1fbd3421e53b68e5c8bb0cc50c
Tia Noel <tiagolson@gmail.com>
2026-02-13 18:38:18 -0500

    restructured imports

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,9 +3,13 @@ import { registerUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
+// Register
 router.post("/register", registerUser);
+
+// Login
 router.post("/login", loginUser);
 
 export default router;
+
 
 
