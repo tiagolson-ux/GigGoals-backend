96461fdd10f59e78d05dba36a463746f5a7fe079
Tia Noel <tiagolson@gmail.com>
2026-02-13 19:10:43 -0500

    updated the user js to debug

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
 // Software T: Define User schema structure
 const userSchema = new mongoose.Schema(
@@ -7,7 +6,7 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
 
     email: {
@@ -15,38 +14,18 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
 
     password: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
-
-// Software T: Hash password before saving to database
-
-  // Software T: Only hash if password was modified
-  if (!this.isModified("password")) {
-    return next();
-  }
-
-  const salt = await bcrypt.genSalt(10);
-
-  // Software T: Replace plain password with hashed version
-  this.password = await bcrypt.hash(this.password, salt);
-
-  next();
-});
-
-// Software T: Method to compare entered password with stored hash
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 const User = mongoose.model("User", userSchema);
 
