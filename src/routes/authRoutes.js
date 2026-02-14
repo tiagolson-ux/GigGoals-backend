493d69943659af0a8e144ef1680694adf55fa6f2
Tia Noel <tiagolson@gmail.com>
2026-02-14 11:47:56 -0500

    add import to routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,20 +2,19 @@ import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
+const router = express.Router();
+
+// Software T: Register route
+router.post("/register", registerUser);
+
+// Software T: Login route
+router.post("/login", loginUser);
+
+// Software T: Protected profile route
 router.get("/profile", protect, (req, res) => {
   res.json(req.user);
 });
 
-
-const router = express.Router();
-
-// Register
-router.post("/register", registerUser);
-
-// Login
-router.post("/login", loginUser);
-
 export default router;
-
 
 
